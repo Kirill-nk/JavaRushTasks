41ac19c5060c0e0db19b33d5ffda059d05b4dd0d
Kirill <zalogi94@gmail.com>
2022-08-04 00:03:21 +0300

    add solution from java syntax task 0109

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+    }
+}
