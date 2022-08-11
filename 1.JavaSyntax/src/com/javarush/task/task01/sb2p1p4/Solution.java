657491f82bd0f54cdab33d2c224d5b2ad69075b1
Kirill <zalogi94@gmail.com>
2022-08-11 23:30:36 +0300

    add solution from JavaSyntax task 0109

--- 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p4/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p4/Solution.java
@@ -3,12 +3,15 @@ package com.javarush.task.task01.sb2p1p4;
 public class Solution {
     public static void main(String[] args) {
         Jedi jedi1 = new Jedi();
+        Jedi jedi2 = new Jedi();
+        Jedi jedi3 = new Jedi();
         jedi1.name = "Obi-Wan";
-        //напишите тут ваш код
+        jedi2.name = "Anakin";
+        jedi3.name = "Joda";
+
     }
 
     public static class Jedi {
-
-
         public String name;
     }
+}
