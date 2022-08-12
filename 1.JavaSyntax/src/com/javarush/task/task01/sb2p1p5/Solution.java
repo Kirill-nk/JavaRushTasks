f8d6d4251215950f916abfd5fd8e0647b87ae94a
Kirill <zalogi94@gmail.com>
2022-08-12 23:43:37 +0300

    add solution from JavaSyntax task 0109_xbz

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p5/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task01.sb2p1p5;
+
+public class Solution {
+    public static void main(String[] args) {
+        Clone clone0 = new Clone();
+        Clone clone1 = new Clone();
+        Clone clone2 = new Clone();
+        Clone clone3 = new Clone();
+        Clone clone4 = new Clone();
+        Clone clone5 = new Clone();
+        Clone clone6 = new Clone();
+        Clone clone7 = new Clone();
+        Clone clone8;
+        Clone clone9;
+    }
+    public static class Clone {
+
+    }
+
+}
