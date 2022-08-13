ab6e07ac6ede3b400fbeadd675370ec3f83ff2b3
Kirill <zalogi94@gmail.com>
2022-08-13 22:06:43 +0300

    Добавил решение задачи 6 второй раздел.

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p7/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task01.sb2p1p7;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+    public static double getWeight(int weight) {
+        return (weight * 0.17);
+
+    }
+}
