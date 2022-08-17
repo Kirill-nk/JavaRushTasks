efaad806ac2219e69bd1c0f6816c79f0b77ff3e7
Kirill <zalogi94@gmail.com>
2022-08-17 22:37:12 +0300

    Добавил sb2p1p2 На живца.

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p2/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.sb2p1p2;
+
+public class Solution {
+    public static void main(String[] args) {
+        increaseSpeed(700);
+    }
+
+    public static void increaseSpeed(int n) {
+        System.out.println("Your speed is: " + (n+100) + " km/h");
+
+    }
+}
+
+
