399ac1dde86aaa1f28b6e997232975e00f8508d1
Kirill <zalogi94@gmail.com>
2022-08-17 22:12:43 +0300

    Добавил sb2p1p1 10 лет спустя.

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p1p1/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.sb2p1p1;
+
+public class Solution {
+    public static void main(String[] args) {
+        print("The power is easy to use!");
+        print("The power opens many opportunities!");
+    }
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
