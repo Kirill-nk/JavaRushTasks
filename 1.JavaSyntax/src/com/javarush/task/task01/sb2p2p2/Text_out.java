598966b925d8b1c8b77a8b9a80aa633756d447c4
Kirill <zalogi94@gmail.com>
2022-08-19 23:06:56 +0300

    Добавил sb2p2p2 Еще шаг с теорией

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p2p2/Text_out.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.sb2p2p2;
+
+import java.util.*;
+
+public class Text_out {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int digit = scanner.nextInt();
+
+        System.out.println(digit);
+
+    }
+}
