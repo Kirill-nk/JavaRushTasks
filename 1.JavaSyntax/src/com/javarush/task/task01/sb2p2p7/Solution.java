42d995dd1cb675e5b195ef318f3380a79c59adb3
Kirill <zalogi94@gmail.com>
2022-08-24 23:10:42 +0300

    коммент

--- 1.JavaSyntax/src/com/javarush/task/task01/sb2p2p7/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p2p7/Solution.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+//почему 212 только в ответе так
         int jedis = 212;
         Scanner scanner = new Scanner(System.in);
         int joda = scanner.nextInt();
         System.out.println(jedis - joda);
-
     }
 }
