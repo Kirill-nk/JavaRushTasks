51bc9d40b5e8c3a5a71421fd03d28f96f41a02eb
Kirill <zalogi94@gmail.com>
2022-08-24 23:05:04 +0300

    sb2p2p7 Задание + решение + ответ

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb2p2p7/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.sb2p2p7;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int jedis = 212;
+        Scanner scanner = new Scanner(System.in);
+        int joda = scanner.nextInt();
+        System.out.println(jedis - joda);
+    }
+}
