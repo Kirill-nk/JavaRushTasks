aa01c64b1dfabb47f135a5145dbf4c6987a8699c
Kirill <zalogi94@gmail.com>
2022-08-30 23:00:22 +0300

    p3p1p5 Задание + решение = легкое

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb3p1p5/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.sb3p1p5;
+
+public class Solution {
+    public static void main(String[] args) {
+        int xw = 100;
+        while (xw >= 0) {
+
+            System.out.println(xw--);
+        }
+
+    }
+}
