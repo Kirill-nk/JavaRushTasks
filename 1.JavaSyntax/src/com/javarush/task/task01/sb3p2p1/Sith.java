0c8fa5a58d048a246f893697a5e8a68217542c14
Kirill <zalogi94@gmail.com>
2022-09-04 23:24:24 +0300

    p3p2p1 Задание+решение

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb3p2p1/Sith.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.sb3p2p1;
+
+public class Sith {
+    public String name;
+    int age;
+    public String race;
+    public int height;
+
+    public static void main(String[] args) {
+
+    }
+}
