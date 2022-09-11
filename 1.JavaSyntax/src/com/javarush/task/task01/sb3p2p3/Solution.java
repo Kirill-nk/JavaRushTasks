9774386c7f8f72887029733376aa4b6c22250b7a
Kirill <zalogi94@gmail.com>
2022-09-11 21:59:18 +0300

    p3p2p3 Геттеры-Сеттеры continue

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/sb3p2p3/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.sb3p2p3;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+    public static class Jedi {
+        private String name;
+        private int age;
+        private String race;
+        private int height;
+        public Jedi(String name, int age, int height, String race) {
+            this.name = name;
+            this.age = age;
+            this.race = race;
+            this.height = height;
+        }
+    }
+}
