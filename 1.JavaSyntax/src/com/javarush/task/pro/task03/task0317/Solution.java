82310493904665bf3a56b7d8c8a16b1af13f0c61
Kirill <zalogi94@gmail.com>
2022-10-10 23:48:15 +0300

    Про температуру, Else Is

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        if (a <= 0);
+            else if (a > 0)
+            System.out.println(a);
+        if (b <= 0) ;
+        else if (b > 0)
+            System.out.println(b);
+        if (c <= 0) ;
+        else if (c > 0)
+            System.out.println(c);
+        if (d <= 0) ;
+        else if (d > 0)
+            System.out.println(d);
+
+
+    }
+}
