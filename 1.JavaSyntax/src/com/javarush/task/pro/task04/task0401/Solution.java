f56a15b2940f30c4efbac6c120985b0cc2afe61f
Kirill <zalogi94@gmail.com>
2022-10-16 21:56:54 +0300

    Задача на цыклы while. Простые

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -7,11 +7,10 @@ package com.javarush.task.pro.task04.task0401;
 public class Solution {
     public static void main(String[] args) {
         String quote = "Я никогда не буду работать за копейки. Амиго";
-        int i = 0;
-
-        while (i<100);
-
-        System.out.println();
-
+        int n = 0;
+        while (n < 100) {
+            System.out.println(quote);
+            n++;
+        }
     }
 }
