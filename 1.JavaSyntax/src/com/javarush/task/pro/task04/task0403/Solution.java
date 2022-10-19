8b7f0d3c5dffceafefd74333ee9d0505fe308bdd
Kirill <zalogi94@gmail.com>
2022-10-19 22:10:54 +0300

    Задача на цыклы while. Задача Q

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -10,16 +10,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        boolean enter = false;
 
-        while (!enter) {
-            String sum = scanner.nextLine();
-            enter = sum.equals("enter");
-        }
+            while (scanner.hasNextInt()) {
+                int x = scanner.nextInt();
+                sum = sum + x;
 
-        while (scanner.hasNextInt()) {
-            int x = scanner.nextInt();
-            sum = sum + x;
 
         }
         System.out.println(sum);
