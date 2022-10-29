10fae1c3e88e79de35b40c2483f7a01cc3aec1ad
Kirill <zalogi94@gmail.com>
2022-10-29 23:04:04 +0300

    Задача на цыклы while. Сумма четных чисел от 1 до 100, решил, Сам, удивлен... :)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0421/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0421;
+
+/* 
+Охлаждение ядра планеты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 100) {
+            if (i % 2 != 0) {
+                i++;
+                continue;
+            }
+            sum = sum + i++;
+        }
+        System.out.println(sum);
+    }
+}
