0ac847a2d1e8db70053a50e9b10ea95c9fea7218
Kirill <zalogi94@gmail.com>
2022-11-18 22:08:24 +0300

    Уровень 6, массивы. Про парные танки, не решил

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0524/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task05.task0524;
+
+/* 
+Взлом Кибердракона
+*/
+
+public class Solution {
+    public static int[] coordinates;
+
+    public static void main(String[] args) {
+        int[] array = new int[];
+        for (int i = 0; i < array.length; i++) {
+
+            
+        }
+    }
+}
