5fabd7ca674605783d608a56a8d4a156c767dc01
Kirill <zalogi94@gmail.com>
2022-11-27 10:49:00 +0300

    Уровень 6. Задача про заполнение с консоли, вывод массива в обратном порядке. Не решил.

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -9,11 +9,21 @@ Reverse
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] array = new int[10];
-        for (int i = 0; i < 10; i++) {
-            array[i] = i;
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+                array[i] = scanner.nextInt();
+            }
         }
-
-        System.out.println(array);
+        if (number % 2 != 0) {
+            for (int i = 0; i < number; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } else {
+            for (int i = number - 1; i >= 0; i--) {
+                System.out.print(array[i] + " ");
+            }
+        }
     }
 }
