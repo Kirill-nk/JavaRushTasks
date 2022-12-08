14fde0f52afafa2faf52f5d3c606cff4c5e63de5
Kirill <zalogi94@gmail.com>
2022-12-08 20:52:01 +0300

    Уровень 6. Задача про заполнение с консоли Строки, вывод 6 и удалить дубликаты. Не решено! Не могу решить пятый день.

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -12,18 +12,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        strings = new String[6];
+        strings[] = new String();
         for (int i = 0; i < 6; i++) {
             strings[i] = console.nextLine();
 
-            boolean b = false;
-            for (int i = 0; i < 6; i++) {
-                if (strings[i] != null) {
-                else strings[i] = null;
-                    }
-                    System.out.print(strings[j] + ", ");
-                }
-            }
+            System.out.print(strings[i] + ", ");
         }
+
     }
 }
