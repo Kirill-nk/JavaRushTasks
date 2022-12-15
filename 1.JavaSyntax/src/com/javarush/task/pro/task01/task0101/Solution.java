6d2bcba441cb891829dd92ed17ef1aa4a193651b
Kirill <zalogi94@gmail.com>
2022-12-15 22:13:30 +0300

    Уровень 7. Не задача, отрывок из джавараша, проверка из лекции.

--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -4,9 +4,17 @@ package com.javarush.task.pro.task01.task0101;
 Первая программа
 */
 
-public class Solution {
-
-    public static void main(String[] args) {
-        System.out.println("Диего + Амиго = Друзья");
+class Solution
+{
+    public static void main(String[] args)
+    {
+        printWiFi();
+        printWiFi();
+        printWiFi();
+    }
+    public static void printWiFi()
+    {
+        System.out.print("Wi-");
+        System.out.println("Fi");
     }
 }
