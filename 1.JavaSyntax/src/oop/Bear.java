5cf0975aaa1d5e7408e5a9d1d17e4228385971f8
Kirill <zalogi94@gmail.com>
2022-12-24 10:14:24 +0300

    Удалил из проекта и перенес в Практику

--- 1.JavaSyntax/src/oop/Bear.java
+++
@@ -1,8 +0,0 @@
-package oop;
-
-public class Bear extends Animal {
-    @Override
-    public String sayHello() {
-        return "rrrr..." + ", my name is " + getNickname() + ", my weight " + getWeight() + " kg";
-    }
-}
--- 1.JavaSyntax/src/oop/Cat.java
+++
@@ -1,8 +0,0 @@
-package oop;
-
-public class Cat extends Animal{
-    @Override
-    public String sayHello() {
-        return "Myau-myau" + ", my name is " + getNickname() + ", my weight " + getWeight() + " kg";
-    }
-}
