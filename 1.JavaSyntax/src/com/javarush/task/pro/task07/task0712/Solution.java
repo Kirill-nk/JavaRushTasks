66ab456c564d1f54d54a951eb644fd62fdfe0515
Kirill <zalogi94@gmail.com>
2023-01-02 12:41:45 +0300

    Задача 0712. Сам. Создание переменных класса с помощью new.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String one = new String();
+        String two = new String();
+        String three = new String();
+        String fur = new String();
+        String five = new String();
+        String six = new String();
+        String seven = new String();
+        String eight = new String();
+        String nine = new String();
+        String ten = new String();
+        int[] eleven = new int[5];
+        int[] twelve = new int[5];
+        int[] thiten = new int[5];
+        int[] forteen = new int[5];
+        int[] fifteen = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        Scanner consol = new Scanner(System.in);
+    }
+}
