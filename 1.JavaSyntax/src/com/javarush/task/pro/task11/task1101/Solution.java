c5e37c4ef3d759e74740d7ff38537c78bdbbe467
Kirill <zalogi94@gmail.com>
2023-02-06 21:43:47 +0300

    Задача 1101. Сам. Просто подставить данные в соут. легко

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1101/Solution.java
@@ -9,11 +9,11 @@ public class Solution {
     public static void main(String[] args) {
 
         System.out.println("Человечество живет в Солнечной системе.");
-        System.out.println("Ее возраст около 4568200000 лет.");
-        System.out.println("В Солнечной системе 8 известных планет.");
-        System.out.println("Как и большинство звездных систем, состоит из 1 звезды.");
-        System.out.println("Звезды по имени Солнце.");
-        System.out.println("Расстояние к центру галактики составляет 27170 световых лет.");
+        System.out.println("Ее возраст около "+ SolarSystem.age +" лет.");
+        System.out.println("В Солнечной системе "+ SolarSystem.planetsCount +" известных планет.");
+        System.out.println("Как и большинство звездных систем, состоит из " + SolarSystem.starsCount + " звезды.");
+        System.out.println("Звезды по имени "+ SolarSystem.starName+".");
+        System.out.println("Расстояние к центру галактики составляет " + SolarSystem.galacticCenterDistance + " световых лет.");
         System.out.println("Каждый обитатель Солнечной системы должен знать эту информацию!");
 
     }
