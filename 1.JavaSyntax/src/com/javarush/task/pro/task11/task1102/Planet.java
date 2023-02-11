8be460ce73e4d6356b805d59b1c204c5804f48f5
Kirill <zalogi94@gmail.com>
2023-02-11 22:43:37 +0300

    Задача 1102. Статик перевести в нестатик. Решил с подсмотра комментариев.

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Planet.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Planet.java
@@ -6,13 +6,13 @@ package com.javarush.task.pro.task11.task1102;
 
 public class Planet {
 
-    public static String name;
+    public String name;
 
-    public static long age;
+    public long age;
 
-    public static int speed;
+    public int speed;
 
-    public static int area;
+    public int area;
 
     public void printInformation() {
         System.out.println("Имя планеты: " + name + ".");
