4c09841a24aeaa5dbe9396ba59f3433c8bd02913
Kirill <zalogi94@gmail.com>
2023-02-15 22:17:51 +0300

    Задача 1025. Сам наполовину. Не сдал. Для доступа к ретранслятору требуется изменить его траекторию и частоту для коммуникаций

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1025/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1025;
+
+/* 
+Взлом ретранслятора
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Repeater repeater = new Repeater();
+
+    }
+}
