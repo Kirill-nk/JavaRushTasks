57c86a1ef0a35b4a1e6b9152ae6836f7393504c8
Kirill <zalogi94@gmail.com>
2023-03-05 09:11:48 +0300

    Задача 0529часть5. Додбиваем робатанки в массиве field

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -25,28 +25,33 @@ public class Solution {
             int j = (int) (Math.random() * width);
             field[i][j] = robotank;
         }
-        for (int i = 0; i < bombs.length; i++) {
-            for (int j = 0; j < 10; ) {
-                int k = (int) (Math.random() * width);
-                if (bombs[i][k] == 0){
-                    bombs[i][k] = 1;
-                j++;
+        int count = 10;
+        while (count > 0) {
+            bombs = new int[height][width];
+            for (int i = 0; i < bombs.length; i++) {
+                for (int j = 0; j < 10; ) {
+                    int k = (int) (Math.random() * width);
+                    if (bombs[i][k] == 0) {
+                        bombs[i][k] = 1;
+                        j++;
+                    }
                 }
             }
-        }
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (field[i][j].equals(robotank) && bombs[i][j] == 1){
-                    field[i][j] = hit;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (field[i][j].equals(robotank) && bombs[i][j] == 1) {
+                        field[i][j] = hit;
+                        count--;
+                    }
+
                 }
 
             }
-
         }
 
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[i].length; j++) {
-                System.out.print(field[i][j]);
+        for (String[] strings : field) {
+            for (String string : strings) {
+                System.out.print(string);
             }
             System.out.println();
         }
