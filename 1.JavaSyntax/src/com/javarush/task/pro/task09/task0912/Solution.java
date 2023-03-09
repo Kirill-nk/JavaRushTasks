6021df9fd83f1670485a7c98753d5d47a379ef7d
Kirill <zalogi94@gmail.com>
2023-03-09 22:57:40 +0300

    Задача 0912. Запустил задачу про поиск урл, но не придумал решение, в поиске.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0912/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task09.task0912;
+
+/* 
+Проверка URL-адреса
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+
+        return "неизвестный";
+    }
+
+    public static String checkDomain(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
+}
