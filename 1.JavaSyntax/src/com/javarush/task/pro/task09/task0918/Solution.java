48af48fff950428c6cf71438600d4a265f72a023
Kirill <zalogi94@gmail.com>
2023-04-05 22:20:17 +0300

    Не решены. В копилку

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0918/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task09.task0918;
+
+/* 
+Поработаем со StringBuilder
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        //напишите тут ваш код
+        return null;
+    }
+}
