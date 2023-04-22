ae5c6ef1fc835a1877fda7117c2ce47e09fa83b9
Kirill <zalogi94@gmail.com>
2023-04-22 09:53:33 +0300

    Задача 1212. Решение

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1212/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1212/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.pro.task12.task1212;
 
+import java.util.ArrayList;
+
 public class Solution {
 
     public static void main(String[] args) {
         CustomStringArrayList arrayList = new CustomStringArrayList();
-        for(int i = 0; i < 45; i++) {
+        for(int i = 0; i < 25; i++) {
             arrayList.add("count" + i);
         }
     }
