94c6b55f88af509327524d3f61861b5df0b5083b
Kirill <zalogi94@gmail.com>
2023-05-27 23:27:45 +0300

    Задача 1304. Попытка разобраться.

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1225/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1225/Solution.java
@@ -22,6 +22,8 @@ public class Solution {
     }
 
     public static void registerAccount(String username) {
-        //напишите тут ваш код
+        //В методе registerAccount класса Solution проверить входящий
+        // username на наличие его в списке accountsList и, если такового в этом списке нет,
+        // добавить username в список accountsList
     }
 }
