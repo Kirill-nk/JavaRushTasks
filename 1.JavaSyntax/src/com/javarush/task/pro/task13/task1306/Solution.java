4a3561995efac50dc0cab11f9e8ee733eaefe37f
Kirill <zalogi94@gmail.com>
2023-06-23 23:08:54 +0300

    Задача 1306. Решил, с помощью, оказалось не трудно!

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1306/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1306/Solution.java
@@ -10,28 +10,14 @@ import java.util.Collections;
 public class Solution {
 
     public static void copy(ArrayList<String> destination, ArrayList<String> source) {
-        ArrayList<Integer> srcList = new ArrayList<Integer>();
-        Collections.addAll(srcList, 1,2,3,4,5,6,7,8,9);
-        if(destination.size() < source.size()) {
-            throw new IndexOutOfBoundsException("Source does not fit in dest");
-        }
-        for (int i = 0; i < source.size(); i++) {
-            destination.set(i, source.get(i));
-        }
-    }
+        Collections.copy(destination, source);
+     }
 
     public static void addAll(ArrayList<String> list, String... strings) {
-        for (String string : strings) {
-            list.add(string);
-        }
+        Collections.addAll(list, strings);
     }
 
     public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
-        for (int i = 0; i < list.size(); i++) {
-            String string = list.get(i);
-            if(string.equals(oldValue)) {
-                list.set(i, newValue);
-            }
-        }
+        Collections.replaceAll(list, oldValue, newValue);
     }
 }
