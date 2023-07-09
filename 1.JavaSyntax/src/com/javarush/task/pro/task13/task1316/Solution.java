00f415bf59a3465acd53b8173e44100896c06882
Kirill <zalogi94@gmail.com>
2023-07-09 22:25:45 +0300

    Задача 1316. Enum продолжение!

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] javarushQuests = JavarushQuest.values();
+        for (JavarushQuest javarushQuest : javarushQuests) {
+            System.out.println(javarushQuest.ordinal());
+        }
+    }
+}
+
