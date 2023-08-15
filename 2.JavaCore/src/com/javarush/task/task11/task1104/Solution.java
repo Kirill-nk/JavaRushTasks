3ab21cd75edbc2e91d7d2c93f9d9267c55027241
Kirill <zalogi94@gmail.com>
2023-08-15 22:29:44 +0300

    Задача 1104 //Вы решили задачу лучше, чем 6% учеников. // Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.07. // Всего эту задачу решили 50134 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1104/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task11.task1104;
+
+/* 
+Хороши работнички
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee{
+
+    }
+}
+
+
+//Вы решили задачу лучше, чем 6% учеников.
+// Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.07.
+// Всего эту задачу решили 50134 учеников.
