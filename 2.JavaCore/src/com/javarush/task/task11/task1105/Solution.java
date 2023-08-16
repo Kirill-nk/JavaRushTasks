fcea046b6e53b414de50b1fa751278b83c0bd341
Kirill <zalogi94@gmail.com>
2023-08-16 21:43:51 +0300

    Задача 1105 //Вы решили задачу лучше, чем 9% учеников. //Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.11. //Всего эту задачу решили 50183 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1105/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task11.task1105;
+
+/* 
+ИТ-компания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class Clerk extends Worker{
+
+    }
+
+    public class IT extends Worker{
+
+    }
+
+    public class Programmer extends IT {
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class CTO extends IT{
+
+    }
+
+    public class OfficeManager extends Clerk{
+
+    }
+
+    public class HR extends Clerk{
+
+    }
+
+    public class Cleaner extends Clerk{
+
+    }
+}
+
+//Вы решили задачу лучше, чем 9% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.11.
+//Всего эту задачу решили 50183 учеников.
