b97339c3c755c8035d3e771bdc683581683c52fd
Kirill <zalogi94@gmail.com>
2023-08-24 22:41:08 +0300

    //1112 //Вы решили задачу лучше, чем 1% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.01. Всего эту задачу решили 49351 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1112/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task11.task1112;
+
+/* 
+iPhone и Apple
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple{
+
+    }
+}
+//1112
+//Вы решили задачу лучше, чем 1% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.01. Всего эту задачу решили 49351 учеников.
