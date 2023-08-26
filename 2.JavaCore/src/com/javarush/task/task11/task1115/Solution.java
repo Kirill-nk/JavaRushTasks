f13af9760365e0dac83e4caee2a8d99c2b726c1a
Kirill <zalogi94@gmail.com>
2023-08-26 22:01:51 +0300

    //1115 //Вы решили задачу лучше, чем 3% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.03. Всего эту задачу решили 49010 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1115/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task11.task1115;
+
+/* 
+От школьника до пенсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy{
+
+    }
+
+    public class Worker extends Student{
+
+    }
+
+    public class Retiree extends Worker{
+
+    }
+
+}
+
+//Вы решили задачу лучше, чем 3% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.03. Всего эту задачу решили 49010 учеников.
