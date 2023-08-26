332b0a0da5a9a6b83495d8ec5026e4fadfe7cafa
Kirill <zalogi94@gmail.com>
2023-08-26 12:18:11 +0300

    //1114 //Вы решили задачу лучше, чем 2% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.01. Всего эту задачу решили 49081 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1114/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task11.task1114;
+
+/* 
+Происхождение видов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish{
+
+    }
+
+    public class Dinosaur extends Lizard{
+
+    }
+
+}
+
+//Вы решили задачу лучше, чем 2% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.01. Всего эту задачу решили 49081 учеников.
