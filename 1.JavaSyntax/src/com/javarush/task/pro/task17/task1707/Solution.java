94d7ccce022183c622752d01da8f405e5bf31bdd
Kirill <zalogi94@gmail.com>
2023-09-03 22:42:03 +0300

    //1707 //Вы решили задачу лучше, чем 2% учеников. // Вам удалось ее решить с 3 попытки. // Среднее количество попыток для этой задачи 1.21. // Всего эту задачу решили 8828 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task17.task1707;
+
+/* 
+Питомцы бывают разные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.printInfo();
+        Dog dog = new Dog();
+        dog.printInfo();
+    }
+}
+
+//1707
+//Вы решили задачу лучше, чем 2% учеников.
+// Вам удалось ее решить с 3 попытки.
+// Среднее количество попыток для этой задачи 1.21.
+// Всего эту задачу решили 8828 учеников.
