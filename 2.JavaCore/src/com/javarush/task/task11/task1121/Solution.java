3e577229073cd026da59663218285f0bf440f7aa
Kirill <zalogi94@gmail.com>
2023-09-06 22:11:47 +0300

    //1121 //Вы решили задачу лучше, чем 12% учеников. Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.14. Всего эту задачу решили 48865 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1121/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task11.task1121;
+
+/* 
+Чужой код, незнакомый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
+//1121
+//Вы решили задачу лучше, чем 12% учеников. Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.14. Всего эту задачу решили 48865 учеников.
