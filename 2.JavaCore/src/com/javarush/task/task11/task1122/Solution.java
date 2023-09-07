33a7be91a3a0d5820d83628030aac9a43a31d9ac
Kirill <zalogi94@gmail.com>
2023-09-07 19:41:49 +0300

    //1122 //Вы решили задачу лучше, чем 8% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.1. Всего эту задачу решили 48749 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1122/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task11.task1122;
+
+/* 
+Помощь шахматной школе
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class King extends ChessPieces{
+    }
+
+    public class Queen extends ChessPieces{
+    }
+
+    public class Rook extends ChessPieces{
+    }
+
+    public class Knight extends ChessPieces{
+    }
+
+    public class Bishop extends ChessPieces{
+    }
+
+    public class Pawn extends ChessPieces{
+    }
+
+    public class ChessPieces {
+    }
+}
+//1122
+//Вы решили задачу лучше, чем 8% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.1. Всего эту задачу решили 48749 учеников.
