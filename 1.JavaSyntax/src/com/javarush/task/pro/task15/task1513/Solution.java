36a1baf42ba677b9c79c244ccc1d6a5717df778d
Kirill <zalogi94@gmail.com>
2023-09-10 18:06:18 +0300

    //1513 //Вы решили задачу лучше, чем 23% учеников. // Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.36. // Всего эту задачу решили 10126 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1513/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task15.task1513;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Зри в корень
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path path = Path.of(str).getRoot();
+        System.out.println(path);
+    }
+}
+
+//1513
+//Вы решили задачу лучше, чем 23% учеников.
+// Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.36.
+// Всего эту задачу решили 10126 учеников.
