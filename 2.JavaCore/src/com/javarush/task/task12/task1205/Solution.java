af1c52069a1541e396af7e49f71a806eb031a383
Kirill <zalogi94@gmail.com>
2023-09-21 22:53:14 +0300

    //1205 //Вы решили задачу лучше, чем 23% учеников. Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.47. Всего эту задачу решили 48852 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1205/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task12.task1205;
+
+/* 
+Определимся с животным
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Dog()));
+        System.out.println(getObjectType(new Whale()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        if (o instanceof Dog) {
+            return "Собака";
+        } else if (o instanceof Cow) {
+            return "Корова";
+        } else if (o instanceof Whale) {
+            return "Кит";
+        } else if (o instanceof Pig) {
+
+            return "Неизвестное животное";
+        }
+        return null;
+    }
+    public static class Cow {
+    }
+
+    public static class Dog {
+    }
+
+    public static class Whale {
+    }
+
+    public static class Pig {
+    }
+}
+//1205
+//Вы решили задачу лучше, чем 23% учеников. Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.47. Всего эту задачу решили 48852 учеников.
