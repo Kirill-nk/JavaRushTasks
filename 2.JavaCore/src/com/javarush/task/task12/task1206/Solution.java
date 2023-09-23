c9de0dfb071d8b3569ae5cae34d1d32146b9460b
Kirill <zalogi94@gmail.com>
2023-09-23 11:04:50 +0300

    //1206 //Вы решили задачу лучше, чем 33% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.55. Всего эту задачу решили 49051 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+    public static void print(String string) {
+
+    }
+}
+
+//1206
+//Вы решили задачу лучше, чем 33% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.55. Всего эту задачу решили 49051 учеников.
