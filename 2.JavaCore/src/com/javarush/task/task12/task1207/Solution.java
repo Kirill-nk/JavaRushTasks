f5b49526e0f4429fffd77f41a7292442cc0c790b
Kirill <zalogi94@gmail.com>
2023-09-24 19:06:39 +0300

    //1207 //Вы решили задачу лучше, чем 3% учеников. Вам удалось ее решить с 5 попытки. // Среднее количество попыток для этой задачи 1.87. Всего эту задачу решили 48796 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution.print(6);
+        Solution.print((Integer) 6);
+
+    }
+
+    public static void print(int o) {
+    }
+    public static void print(Integer p) {
+    }
+}
+
+//1207
+//Вы решили задачу лучше, чем 3% учеников. Вам удалось ее решить с 5 попытки.
+// Среднее количество попыток для этой задачи 1.87. Всего эту задачу решили 48796 учеников.
