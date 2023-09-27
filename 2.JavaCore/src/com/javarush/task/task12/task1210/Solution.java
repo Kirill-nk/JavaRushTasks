5eedf67904ad1f557ca66bd35d2307b552d08554
Kirill <zalogi94@gmail.com>
2023-09-27 22:06:06 +0300

    //1210 //Вы решили задачу лучше, чем 15% учеников. Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.19. Всего эту задачу решили 48363 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int a, int b){
+        return Math.max(a, b);
+    }
+    public static long max(long a, long b){
+        return Math.max(a, b);
+    }
+    public static double max(double a, double b){
+        return Math.max(a, b);
+    }
+
+}
+//1210
+//Вы решили задачу лучше, чем 15% учеников. Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.19. Всего эту задачу решили 48363 учеников.
