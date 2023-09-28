aeb42df23c3a09de363b46a7a1e6ca55554548cd
Kirill <zalogi94@gmail.com>
2023-09-28 21:01:06 +0300

    //1211 //Вы решили задачу лучше, чем 2% учеников. Вам удалось ее решить с 2 попытки. //Среднее количество попыток для этой задачи 1.2. Всего эту задачу решили 48864 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1211/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1211;
+
+/* 
+Абстрактный класс Pet
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+}
+//1211
+//Вы решили задачу лучше, чем 2% учеников. Вам удалось ее решить с 2 попытки.
+//Среднее количество попыток для этой задачи 1.2. Всего эту задачу решили 48864 учеников.
