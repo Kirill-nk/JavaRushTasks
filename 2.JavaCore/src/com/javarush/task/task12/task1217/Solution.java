2c3f4688fd3639eb5faed6e0f25d1dcdbb086bad
Kirill <zalogi94@gmail.com>
2023-10-04 19:50:12 +0300

    //1217 //Вы решили задачу лучше, чем 11% учеников. // Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.15. // Всего эту задачу решили 48389 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanFly {
+        void draw();
+    }
+    interface CanRun {
+        void draw();
+    }
+    interface CanSwim {
+        void draw();
+    }
+}
+//1217
+//Вы решили задачу лучше, чем 11% учеников.
+// Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.15.
+// Всего эту задачу решили 48389 учеников.
