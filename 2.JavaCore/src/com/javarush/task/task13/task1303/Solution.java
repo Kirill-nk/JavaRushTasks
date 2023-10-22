0572229511be7f9560875b1147640cc82712f1d3
Kirill <zalogi94@gmail.com>
2023-10-22 21:46:36 +0300

    //1303 //Статики и котики //Вы решили задачу лучше, чем 0% учеников. // Вам удалось ее решить с 11 попытки. // Среднее количество попыток для этой задачи 1.25. // Всего эту задачу решили 47246 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1303/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task13.task1303;
+
+/* 
+Пиво с колой не мешать
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        print(new Beer());
+        print(new Cola());
+    }
+
+    private static void print(Drink drink) {
+        System.out.println(drink.getClass().getSimpleName());
+    }
+
+    public interface Drink {
+        boolean isAlcoholic();
+    }
+
+    public static class Beer implements  Drink {
+        @Override
+        public boolean isAlcoholic() {
+            return true;
+        }
+    }
+
+    public static class Cola implements Drink{
+        @Override
+        public boolean isAlcoholic() {
+            return false;
+        }
+    }
+}
+//1303
+//Вы решили задачу лучше, чем 0% учеников.
+// Вам удалось ее решить с 11 попытки.
+// Среднее количество попыток для этой задачи 1.25.
+// Всего эту задачу решили 47246 учеников.
