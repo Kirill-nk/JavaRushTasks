c22d827735f39f449e3567c89310caec829117f3
Kirill <zalogi94@gmail.com>
2023-10-27 21:05:29 +0300

    //1308 //Эй, ты там живой? Создать интерфейс Person. //1308 //Вы решили задачу лучше, чем 5% учеников. Вам удалось ее решить с 4 попытки. //Среднее количество попыток для этой задачи 1.5 Всего эту задачу решили 46749 учеников.

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1308/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task13.task1308;
+
+/* 
+Эй, ты там живой?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    interface Person {
+        boolean isAlive();
+    }
+    interface Presentable extends Person {
+
+    }
+
+}
+
+//1308
+//Вы решили задачу лучше, чем 5% учеников. Вам удалось ее решить с 4 попытки.
+//Среднее количество попыток для этой задачи 1.5 Всего эту задачу решили 46749 учеников.
