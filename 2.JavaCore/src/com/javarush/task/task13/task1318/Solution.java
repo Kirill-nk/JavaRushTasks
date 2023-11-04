adea151df57283434be1ef5cc834cd486053cad1
Kirill <zalogi94@gmail.com>
2023-11-04 21:51:40 +0300

    //1318 Requirements: 1. Реализуй метод public static Month getNextMonth(Month): он должен работать согласно условию.

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Чтение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new  InputStreamReader(System.in));
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
