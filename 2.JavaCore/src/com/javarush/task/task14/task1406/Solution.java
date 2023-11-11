85a4fab0a790c819a8031213b8d8b15d214d2d4b
Kirill <zalogi94@gmail.com>
2023-11-11 22:55:24 +0300

    //1406 //Вы решили задачу лучше, чем 3% учеников. // Вам удалось ее решить с 3 попытки. // Среднее количество попыток для этой задачи 1.38. // Всего эту задачу решили 43690 учеников.
    Без ошибок
    Давай напишем программу, которая создает мышонка.
    Для этого инициализируй объект obj таким классом, чтобы метод main() выполнился без ошибок.

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1406/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task14.task1406;
+
+/* 
+Без ошибок
+*/
+
+import com.javarush.task.task14.task1406.Solution.Mouse;
+
+public class Solution {
+    public static void main(String[] args) {
+        Object obj = new Jerry();//Add your code here
+
+                Mouse mouse = (Mouse) obj;
+        GrayMouse grayMouse = (GrayMouse) mouse;
+        Jerry jerry = (Jerry) grayMouse;
+
+        printClasses(obj, mouse, grayMouse, jerry);
+
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GrayMouse grayMouse, Jerry jerry) {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(grayMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse {
+    }
+
+    static class GrayMouse extends Mouse {
+    }
+
+    static class Jerry extends GrayMouse {
+    }
+}
+
+//1406
+//Вы решили задачу лучше, чем 3% учеников.
+// Вам удалось ее решить с 3 попытки.
+// Среднее количество попыток для этой задачи 1.38.
+// Всего эту задачу решили 43690 учеников.
