a990b618a0eda23790bdd5b194bc72caf1688113
Kirill <zalogi94@gmail.com>
2023-12-09 17:42:13 +0300

    //1612 //Вы решили задачу лучше, чем 0% учеников. //Вам удалось ее решить с 16 попытки. //Среднее количество попыток для этой задачи 1.28. //Всего эту задачу решили 9036 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1612/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task16.task1612;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+
+/* 
+Синтезируем LocalDateTime
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<LocalDate, List<LocalTime>> dateMap = DateTimeGenerator.generateDateMap();
+        printCollection(dateMap.entrySet());
+
+        Set<LocalDateTime> dateSet = convert(dateMap);
+        printCollection(dateSet);
+    }
+
+    static Set<LocalDateTime> convert(Map<LocalDate, List<LocalTime>> sourceMap) {
+        //напишите тут ваш код
+        Set<LocalDateTime> set = new HashSet<>();
+        for (Map.Entry<LocalDate, List<LocalTime>> payer : sourceMap.entrySet()) {
+            for (LocalTime localTime : payer.getValue()) {
+                LocalDateTime localDateTime = LocalDateTime.of(payer.getKey(), localTime);
+                set.add(localDateTime);
+            }
+
+        }
+        return set;
+    }
+
+    static void printCollection(Collection<?> collection) {
+        System.out.println("-----------------------------------------------------");
+        collection.forEach(System.out::println);
+    }
+}
+//1612
+//Вы решили задачу лучше, чем 0% учеников.
+//Вам удалось ее решить с 16 попытки.
+//Среднее количество попыток для этой задачи 1.28.
+//Всего эту задачу решили 9036 учеников.
