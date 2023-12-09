26ee44e977117ff51f9ffdc66523dc3253343a59
Kirill <zalogi94@gmail.com>
2023-12-09 23:32:45 +0300

    //1614 Задачка не решена

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1614/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task16.task1614;
+
+import java.time.Instant;
+
+/* 
+Конец времен
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getMaxFromMilliseconds());
+        System.out.println(getMaxFromSeconds());
+        System.out.println(getMaxFromSecondsAndNanos());
+    }
+
+    static Instant getMaxFromMilliseconds() {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static Instant getMaxFromSeconds() {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static Instant getMaxFromSecondsAndNanos() {
+        //напишите тут ваш код
+
+        return null;
+    }
+}
