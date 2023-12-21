0cf4f5378af2e3f57c3757570bca96df100bf98c
Kirill-nk <kirill9414@yandex.ru>
2023-12-21 22:16:03 +0300

    //1407 Еда едой, а сон по расписанию Запусти программу и посмотри, что происходит. //Вы решили задачу лучше, чем 17% учеников. //Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.32. //Всего эту задачу решили 13251 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1407/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task14.task1407;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat(new Food("мясо"));
+        lion.eat(null);
+    }
+}
+//1407
+//Вы решили задачу лучше, чем 17% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.32.
+//Всего эту задачу решили 13251 учеников.
