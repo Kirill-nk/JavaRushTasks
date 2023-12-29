de4968515655dd83f388e8afe02d537db9895a6e
Kirill-nk <kirill9414@yandex.ru>
2023-12-29 21:46:18 +0300

    //1614 Конец времен. В классе Solution реализуй три метода: //Вы решили задачу лучше, чем 13% учеников. // Вам удалось ее решить с 3 попытки. // Среднее количество попыток для этой задачи 2.09. // Всего эту задачу решили 9497 учеников.

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1614/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1614/Solution.java
@@ -15,20 +15,23 @@ public class Solution {
     }
 
     static Instant getMaxFromMilliseconds() {
-        //напишите тут ваш код
-
-        return null;
+        Instant timestamp = Instant.ofEpochMilli(Long.MAX_VALUE);
+        return timestamp;
     }
 
     static Instant getMaxFromSeconds() {
-        //напишите тут ваш код
-
-        return null;
+        Instant timestamp = Instant.ofEpochSecond(Instant.MAX.getEpochSecond());
+        return timestamp;
     }
 
     static Instant getMaxFromSecondsAndNanos() {
-        //напишите тут ваш код
-
-        return null;
+        Instant timestamp = Instant.ofEpochSecond(Instant.MAX.getEpochSecond(), Instant.MAX.getNano());
+        return timestamp;
     }
 }
+//1614
+//Конец времен В классе Solution реализуй три метода:
+//Вы решили задачу лучше, чем 13% учеников.
+// Вам удалось ее решить с 3 попытки.
+// Среднее количество попыток для этой задачи 2.09.
+// Всего эту задачу решили 9497 учеников.
