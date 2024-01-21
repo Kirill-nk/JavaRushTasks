2f12466e28054a76e69b90878228655a79bfcc08
Kirill-nk <kirill9414@yandex.ru>
2024-01-21 22:49:58 +0300

    //1709 //Вы решили задачу лучше, чем 21% учеников. //Вам удалось ее решить с 1 попытки. //Среднее количество попыток для этой задачи 1.32. //Всего эту задачу решили 10080 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+public abstract class Shape {
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+}
+//1709
+//Вы решили задачу лучше, чем 21% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.32.
+//Всего эту задачу решили 10080 учеников.
