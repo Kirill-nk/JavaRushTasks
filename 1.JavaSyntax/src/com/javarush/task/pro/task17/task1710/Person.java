5aa149492c95b90764236d6ef590c729e081760f
Kirill-nk <kirill9414@yandex.ru>
2024-01-22 22:02:39 +0300

    //1710 //Вы решили задачу лучше, чем 1% учеников. //Вам удалось ее решить с 5 попытки. //Среднее количество попыток для этой задачи 1.27. //Всего эту задачу решили 10037 учеников.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1710/Person.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task17.task1710;
+
+/* 
+От абстракции к реальности
+*/
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+//1710
+//Вы решили задачу лучше, чем 1% учеников.
+//Вам удалось ее решить с 5 попытки.
+//Среднее количество попыток для этой задачи 1.27.
+//Всего эту задачу решили 10037 учеников.
