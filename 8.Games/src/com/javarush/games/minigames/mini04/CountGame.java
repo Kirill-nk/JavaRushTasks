7c0c11ab1f8c00b65d00fbd578135849f27b805b
Kirill-nk <kirill9414@yandex.ru>
2024-02-17 23:25:49 +0300

    Commit games: 2001, 2002, 2003

---
+++ 8.Games/src/com/javarush/games/minigames/mini04/CountGame.java
@@ -0,0 +1,36 @@
+package com.javarush.games.minigames.mini04;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Считаем клетки
+*/
+
+public class CountGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(10, 10);
+        showGrid(false);
+        for (int j = 0; j < 50; j++) {
+            int x = getRandomNumber(10);
+            int y = getRandomNumber(10);
+            setCellColor(x, y, Color.GREEN);
+            setCellNumber(x, y, getRandomNumber(100));
+        }
+        showResult();
+    }
+
+    public void showResult() {
+        //напишите тут ваш код
+    }
+
+    private void printSum(int sum) {
+        System.out.println("Сумма всех чисел = " + sum);
+    }
+
+    private void printCountOfGreenCells(int count) {
+        System.out.println("Количество зеленых клеток = " + count);
+    }
+}
