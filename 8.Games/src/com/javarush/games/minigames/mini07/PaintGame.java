12bcab432c777c0e286ac9a86c80c3a586a49eb0
Kirill-nk <kirill9414@yandex.ru>
2024-02-23 21:57:41 +0300

    //game07 //Вы решили задачу лучше, чем 26% учеников. // Вам удалось ее решить с 1 попытки. // Среднее количество попыток для этой задачи 1.39. // Всего эту задачу решили 6748 учеников.

---
+++ 8.Games/src/com/javarush/games/minigames/mini07/PaintGame.java
@@ -0,0 +1,38 @@
+package com.javarush.games.minigames.mini07;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Работа с мышью
+*/
+
+public class PaintGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(5, 5);
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                setCellColor(x,y, Color.WHITE);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        setCellColor(x,y, Color.GREEN);
+    }
+
+    @Override
+    public void onMouseRightClick(int x, int y) {
+        setCellColor(x,y, Color.ORANGE);
+    }
+}
+//game07
+//Вы решили задачу лучше, чем 26% учеников.
+// Вам удалось ее решить с 1 попытки.
+// Среднее количество попыток для этой задачи 1.39.
+// Всего эту задачу решили 6748 учеников.
