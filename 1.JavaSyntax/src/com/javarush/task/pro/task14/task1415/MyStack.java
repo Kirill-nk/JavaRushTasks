183ab2b7cd017780a657fb9ce36d76a0227dda82
Kirill-nk <kirill9414@yandex.ru>
2024-03-06 21:49:28 +0300

    1415. решение. Стек в домашних условиях

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
@@ -13,21 +13,37 @@ public class MyStack {
 
     public void push(String s) {
         //напишите тут ваш код
+        storage.add(0, s);
     }
 
     public String pop() {
         //напишите тут ваш код
+        return storage.remove(0);
     }
 
     public String peek() {
         //напишите тут ваш код
+        return storage.get(0);
     }
 
     public boolean empty() {
         //напишите тут ваш код
+        return storage.isEmpty();
     }
 
     public int search(String s) {
         //напишите тут ваш код
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).equals(s)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
+//1415
+//Вы решили задачу лучше, чем 15% учеников.
+//Вам удалось ее решить с 3 попытки.
+//Среднее количество попыток для этой задачи 2.31.
+//Всего эту задачу решили 12805 учеников.
