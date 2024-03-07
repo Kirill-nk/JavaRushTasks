66508be61de8b59e9bacd5d71e2ad28556735894
Kirill-nk <kirill9414@yandex.ru>
2024-03-07 23:24:03 +0300

    1422. Сбор и сканирование ДНК

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1422/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task14.task1422;
+
+/* 
+Сбор и сканирование ДНК
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        printDNA();
+    }
+
+    public static void printDNA() {
+        //напишите тут ваш код
+    }
+
+    private static List<StackTraceElement[]> getDNA() {
+        List<StackTraceElement[]> list = new ArrayList<>();
+
+        list.add(adenine());
+        list.add(thymine());
+        list.add(cytosine());
+        list.add(guanine());
+
+        return list;
+    }
+
+    private static StackTraceElement[] adenine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] thymine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] cytosine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] guanine() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
