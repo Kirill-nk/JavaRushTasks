dfe14848d42d2f79658dde657ed7a90352049740
Kirill-nk <kirill9414@yandex.ru>
2024-03-10 23:12:15 +0300

    1522. Получение информации по API. Решение

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1522/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1522/Solution.java
@@ -12,7 +12,15 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://javarush.ru/api/1.0/rest/projects");
         //напишите тут ваш код
-
-
+        InputStream input = url.openStream();
+        byte[] buffer = input.readAllBytes();
+        String str = new String(buffer);
+        System.out.println(str);
     }
 }
+
+//1522
+//Вы решили задачу лучше, чем 29% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.52.
+//Всего эту задачу решили 10101 учеников.
