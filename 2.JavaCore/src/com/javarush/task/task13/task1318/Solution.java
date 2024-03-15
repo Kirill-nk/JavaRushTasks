9ff8c2008f7419255e8b331baae386bad746afc3
Kirill-nk <kirill9414@yandex.ru>
2024-03-16 00:03:06 +0300

    1318. Чтение файла

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task13.task1318;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 /* 
@@ -10,14 +11,24 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
-        BufferedReader reader = new BufferedReader(new  InputStreamReader(System.in));
 
-    }
+//      System.out.println("Введите путь");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String src = reader.readLine();
 
-    @Override
-    public String toString() {
-        return super.toString();
+
+        try (FileInputStream bufferedReader = new FileInputStream(src)) {
+            int i;
+            while ((i = bufferedReader.read()) != -1) {
+                System.out.print((char) i);
+            }
+        }reader.close();
     }
 }
+//1318
+//Вы решили задачу лучше, чем 10% учеников.
+//Вам удалось ее решить с 8 попытки.
+//Среднее количество попыток для этой задачи 4.15.
+//Всего эту задачу решили 43667 учеников.
