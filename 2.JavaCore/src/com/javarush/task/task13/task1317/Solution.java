86648012034ed05ab028d353e2c66e7ba3dc9873
Kirill-nk <kirill9414@yandex.ru>
2024-03-16 19:53:39 +0300

    1317. The weather is fine. Интерфейсы

--- 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1317/Solution.java
@@ -6,17 +6,17 @@ The weather is fine
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(new Today(WeatherType.CLOUDY));
-        System.out.println(new Today(WeatherType.FOGGY));
-        System.out.println(new Today(WeatherType.FREEZING));
-    }
+//        System.out.println(new Today(WeatherType.CLOUDY));
+//        System.out.println(new Today(WeatherType.FOGGY));
+//        System.out.println(new Today(WeatherType.FREEZING));
+  /*  }
 
-    static class Today implements Weather{
-        private String type;
-
-        Today(String type) {
-            this.type = type;
-        }
+//    static class Today implements Weather{
+//        private String type;
+//
+//        Today(String type) {
+//            this.type = type;
+//        }
 
         @Override
         public String toString() {
@@ -32,9 +32,11 @@ public class Solution {
         }
     }
 }
-
+*/
 //1317
 //Вы решили задачу лучше, чем 0% учеников.
 //Вам удалось ее решить с 15 попытки.
 //Среднее количество попыток для этой задачи 1.41.
 //Всего эту задачу решили 45233 учеников.
+    }
+}
