7a98d6843a8c74bc46dd131bd7c5b5e3037623b2
Kirill-nk <kirill9414@yandex.ru>
2024-03-20 23:00:12 +0300

    1319. Писатель в файл с консоли

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        System.out.println("Введите путь: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//      String src = reader.readLine();
+    }
+}
