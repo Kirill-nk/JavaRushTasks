9a016a43f362d2180c16a443e4015bda6ff65e8c
Kirill-nk <kirill9414@yandex.ru>
2024-03-23 23:38:28 +0300

    1319. Писатель в файл с консоли

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,19 +1,36 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
-        System.out.println("Введите путь: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//      String src = reader.readLine();
+    public static void main(String[] args) throws IOException {
+//      System.out.println("Введите путь: ");
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            while (true) {
+                String text = scanner.nextLine();
+                writer.write(text);
+                writer.newLine();
+                if (text.equals("exit")) {
+                    break;
+                }
+            }
+        }
     }
 }
+
+//1319
+//Вы решили задачу лучше, чем 21% учеников.
+//Вам удалось ее решить с 4 попытки.
+//Среднее количество попыток для этой задачи 3.39.
+//Всего эту задачу решили 42202 учеников.
