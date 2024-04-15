4a828d742197444aba49b717474df00c7032f41e
Kirill-nk <kirill9414@yandex.ru>
2024-04-15 23:04:47 +0300

    1415. Решение

--- 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -19,6 +19,18 @@ public class Solution {
 
     public static void cleanAllApartments(List<Apartment> apartments) {
         //написать тут вашу реализацию пунктов 1-4
+        for(Apartment str : apartments){
+            if(str instanceof OneRoomApt){
+                OneRoomApt one = (OneRoomApt) str;
+                one.clean1Room();
+            } else if (str instanceof TwoRoomApt){
+                TwoRoomApt two = (TwoRoomApt) str;
+                two.clean2Rooms();
+            }else if( str instanceof ThreeRoomApt){
+                ThreeRoomApt three = (ThreeRoomApt) str;
+                three.clean3Rooms();
+            }
+        }
     }
 
     static interface Apartment {
@@ -42,3 +54,8 @@ public class Solution {
         }
     }
 }
+//1415
+//Вы решили задачу лучше, чем 22% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.52.
+//Всего эту задачу решили 40648 учеников.
