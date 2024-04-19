31910e918893e957d26e1ef31338110b0dfb426f
Kirill-nk <kirill9414@yandex.ru>
2024-04-19 23:03:10 +0300

    Game. MineSweaper (4/16)

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -3,11 +3,9 @@ package com.javarush.games.minesweeper;
 public class GameObject {
     public int x;
     public int y;
-    public boolean isMine;
 
-    public GameObject(int x, int y, boolean isMine) {
+    public GameObject(int x, int y) {
         this.x = x;
         this.y = y;
-        this.isMine = isMine;
     }
 }
