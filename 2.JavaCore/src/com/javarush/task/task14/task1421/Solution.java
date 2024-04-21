ac5a247bb8e83bf6c4192e03394bdc23be17ea3c
Kirill-nk <kirill9414@yandex.ru>
2024-04-22 00:22:27 +0300

    1421. Singleton

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task14.task1421;
+
+/* 
+Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Singleton.class.getClass();
+    }
+}
+
+//1421
+//Вы решили задачу лучше, чем 3% учеников.
+//Вам удалось ее решить с 7 попытки.
+//Среднее количество попыток для этой задачи 2.34.
+//Всего эту задачу решили 38551 учеников
