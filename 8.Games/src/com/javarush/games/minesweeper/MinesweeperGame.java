ea6d42c18fc07acffa7fe8190cbabd9770f32622
Kirill-nk <kirill9414@yandex.ru>
2024-04-21 11:30:58 +0300

    Game. MineSweaper (4/16). Решение

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,16 +4,19 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
     public void initialize(){
         setScreenSize(SIDE,SIDE);
-        GameObject[][] gameField = new GameObject[SIDE][SIDE];
         createGame();
     }
     private void createGame(){
         for (int x = 0; x < SIDE; x++) {
             for (int y = 0; y < SIDE; y++) {
+                gameField[y][x] = new GameObject(x, y);
                 setCellColor(x,y, Color.LAWNGREEN);
             }
         }
     }
 }
+//4/16
+//Вы решили задачу лучше, чем 82% учеников. Вам удалось ее решить с 1 попытки. Среднее количество попыток для этой задачи 5.24. Всего эту задачу решили 13934 учеников.
