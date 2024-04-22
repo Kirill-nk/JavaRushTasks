dacdf302c8d6c5cd00b6feba2a16cc31f56669f2
Kirill-nk <kirill9414@yandex.ru>
2024-04-23 00:00:46 +0300

    game. Space Invaders (1/34)

---
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -0,0 +1,19 @@
+package com.javarush.games.spaceinvaders;
+
+import com.javarush.engine.cell.*;
+
+public class SpaceInvadersGame extends Game {
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH,HEIGHT);
+    }
+}
+
+//game. Space Invaders (1/34)
+//Вы решили задачу лучше, чем 15% учеников.
+//Вам удалось ее решить с 2 попытки.
+//Среднее количество попыток для этой задачи 1.93.
+//Всего эту задачу решили 2597 учеников.
