42013a868214e83d07f510c5da2fbd2fdf7bbd21
Kirill-nk <kirill9414@yandex.ru>
2024-04-25 22:48:52 +0300

    //1417. Валюты

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task14.task1417;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Валюты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person ivan = new Person("Иван");
+        for (Money money : ivan.getAllMoney()) {
+            System.out.println(ivan.name + " имеет заначку в размере " + money.getAmount() + " " + money.getCurrencyName());
+        }
+    }
+
+    static class Person {
+        public String name;
+
+        Person(String name) {
+            this.name = name;
+            this.allMoney = new ArrayList<Money>();
+            //напишите тут ваш код
+        }
+
+        private List<Money> allMoney;
+
+        public List<Money> getAllMoney() {
+            return allMoney;
+        }
+    }
+}
+
+//1417. Валюты
+//Вы решили задачу лучше, чем 4% учеников.
+// Вам удалось ее решить с 4 попытки.
+// Среднее количество попыток для этой задачи 1.7.
+// Всего эту задачу решили 39268 учеников.
