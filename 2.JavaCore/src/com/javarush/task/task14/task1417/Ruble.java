ea18cc676613b406225ddc5990158d9e1ff6a5de
Kirill-nk <kirill9414@yandex.ru>
2024-04-25 22:49:05 +0300

    //1417. Валюты

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Ruble.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task14.task1417;
+
+public class Ruble extends Money {
+    public Ruble(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public double getAmount() {
+        return 0;
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "RUB";
+    }
+}
