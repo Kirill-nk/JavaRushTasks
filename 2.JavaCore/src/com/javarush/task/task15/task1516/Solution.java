9d7aaa3d2361e2f5264b9eb34986a2e60afa5016
Kirill-nk <kirill9414@yandex.ru>
2024-04-27 23:53:35 +0300

    //1516. Значения по умолчанию

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1516/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1516;
+
+/* 
+Значения по умолчанию
+*/
+
+public class Solution {
+    public int intVar;
+    public double doubleVar;
+    public Double DoubleVar;
+    public boolean booleanVar;
+    public Object ObjectVar;
+    public Exception ExceptionVar;
+    public String StringVar;
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().intVar);
+        System.out.println(new Solution().doubleVar);
+        System.out.println(new Solution().DoubleVar);
+        System.out.println(new Solution().booleanVar);
+        System.out.println(new Solution().ObjectVar);
+        System.out.println(new Solution().ExceptionVar);
+        System.out.println(new Solution().StringVar);
+    }
+}
+
+//1516
+//Вы решили задачу лучше, чем 27% учеников.
+//Вам удалось ее решить с 2 попытки.
+//Среднее количество попыток для этой задачи 2.28.
+//Всего эту задачу решили 40666 учеников.
