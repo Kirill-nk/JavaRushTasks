d5acf1574380f97a1d88af31b8c9d63f67c44f44
Kirill-nk <kirill9414@yandex.ru>
2024-05-04 14:19:58 +0300

    //1523. Перегрузка конструкторов

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+        super();
+    }
+
+    SubSolution(String s){
+        super();
+
+    }
+
+    protected SubSolution(Integer a){
+        super();
+
+    }
+
+
+}
