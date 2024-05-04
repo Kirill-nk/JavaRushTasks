87eac7e71d00d7cd78d82164e9fd9b70179d9e22
Kirill-nk <kirill9414@yandex.ru>
2024-05-04 14:19:54 +0300

    //1523. Перегрузка конструкторов

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution() {
+
+    }
+
+    Solution(double a){
+
+    }
+
+    protected Solution(Integer a){
+
+    }
+
+    private Solution(char a) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+//1523. Перегрузка конструкторов
+//Вы решили задачу лучше, чем 1% учеников. Вам удалось ее решить с 15 попытки. Среднее количество попыток для этой задачи 2.52. Всего эту задачу решили 37157 учеников.
