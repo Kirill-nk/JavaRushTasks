a2dae4d9e4e146a81a5697c102818e2621740c1c
Kirill-nk <kirill9414@yandex.ru>
2024-05-07 23:16:19 +0300

    //1525. Файл в статическом блоке

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Statics {
+
+    public static String FILE_NAME = "C:\\TheProjects\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task15\\task1525\\1.txt" /* add the path to your source file here */;
+
+
+}
