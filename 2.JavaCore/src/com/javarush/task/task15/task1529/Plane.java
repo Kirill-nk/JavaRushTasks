7e0da5f94f2594b8b33d96b242f71d3f9d43ac04
Kirill-nk <kirill9414@yandex.ru>
2024-05-10 16:18:15 +0300

    //1529. Осваивание статического блока

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+    public Plane(int count) {
+    }
+
+    @Override
+    public int fly() {
+        return 0;
+    }
+}
