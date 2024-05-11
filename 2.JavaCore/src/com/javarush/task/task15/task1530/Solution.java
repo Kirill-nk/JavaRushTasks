9cec9511985c104d91d18adb4d67c2b43af24bf5
Kirill-nk <kirill9414@yandex.ru>
2024-05-11 13:40:26 +0300

    //1530. Template pattern

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1530/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task15.task1530;
+
+/* 
+Template pattern
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        LatteMaker latteMaker = new LatteMaker();
+        latteMaker.getRightCup();
+        latteMaker.putIngredient();
+        latteMaker.pour();
+    }
+}
+
+
+//1530. Template pattern
+//Вы решили задачу лучше, чем 1% учеников. Вам удалось ее решить с 7 попытки. Среднее количество попыток для этой задачи 1.65. Всего эту задачу решили 36035 учеников.
