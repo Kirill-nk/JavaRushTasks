7392d27005b2862fefa5d5caf4a566ee5f4ed44c
Kirill-nk <kirill9414@yandex.ru>
2024-05-16 22:04:47 +0300

    //1608. Продвижение на политических дебатах 07.05.2024

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1608/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task16.task1608;
+
+/* 
+Продвижение на политических дебатах
+*/
+
+public class Solution {
+    public static int totalSpeechCount = 200;
+    public static int utterancesPerSpeech = 1000000;
+
+    public static void main(String[] args) throws InterruptedException {
+        Politician ivanov = new Politician("Иванов");
+        ivanov.join();
+        Politician petrov = new Politician("Петров");
+        Politician sidorov = new Politician("Сидоров");
+
+        while (ivanov.getSpeechCount() + petrov.getSpeechCount() + sidorov.getSpeechCount() < totalSpeechCount) {
+        }
+
+        System.out.println(ivanov);
+        System.out.println(petrov);
+        System.out.println(sidorov);
+    }
+
+    public static class Politician extends Thread {
+        private volatile int utteranceCount;
+
+        public Politician(String name) {
+            super(name);
+            start();
+        }
+
+        public void run() {
+            while (utteranceCount < totalSpeechCount * utterancesPerSpeech) {
+                utteranceCount++;
+            }
+        }
+
+        public int getSpeechCount() {
+            return utteranceCount / utterancesPerSpeech;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s сказал речь %d раз", getName(), getSpeechCount());
+        }
+    }
+}
+
+//1608. Продвижение на политических дебатах 07.05.2024
+//Вы решили задачу лучше, чем 27% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 1.49.
+//Всего эту задачу решили 38984 учеников.
