605f31aa624365ebe705849adec8e6c70fe2b6c8
Kirill-nk <kirill9414@yandex.ru>
2024-05-18 23:46:43 +0300

    //1610. Расставь вызовы методов join().

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1610/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task16.task1610;
+
+/* 
+Расставь вызовы методов join()
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Cat cat1 = new Cat("Мурка");
+        Cat cat2 = new Cat("Пушинка");
+    }
+
+    private static void investigateWorld() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static class Cat extends Thread {
+        protected Kitten kitten1;
+        protected Kitten kitten2;
+
+        public Cat(String name) {
+            super(name);
+            kitten1 = new Kitten("Котенок 1, мама - " + getName());
+            kitten2 = new Kitten("Котенок 2, мама - " + getName());
+            start();
+        }
+
+        public void run() {
+            System.out.println(getName() + " родила 2 котят");
+            try {
+                initAllKittens();
+                kitten1.join();
+                kitten2.join();
+            } catch (InterruptedException e) {
+            }
+            System.out.println(getName() + ": Все котята в корзинке. " + getName() + " собрала их назад");
+        }
+
+        private void initAllKittens() throws InterruptedException {
+            kitten1.start();
+            kitten2.start();
+        }
+    }
+
+    public static class Kitten extends Thread {
+        public Kitten(String name) {
+            super(name);
+        }
+
+        public void run() {
+            System.out.println(getName() + ", вылез из корзинки");
+            investigateWorld();
+        }
+    }
+}
+
+//1610. Расставь вызовы методов join().
+//Вы решили задачу лучше, чем 43% учеников.
+//Вам удалось ее решить с 1 попытки.
+//Среднее количество попыток для этой задачи 2.0.
+//Всего эту задачу решили 38716 учеников.
