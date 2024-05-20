70016139ca96fdd745d0f2df05effc4518a00042
Kirill-nk <kirill9414@yandex.ru>
2024-05-20 19:49:50 +0300

    //1612. Stopwatch (Секундомер)

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1612/Molution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task16.task1612;
+/*
+Просто переписываю задачу
+ */
+
+public class Molution {
+    public static volatile boolean isStoped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runner ivanov = new Runner("Ivanov", 4);
+        Runner petrov = new Runner("Petrov", 2);
+        ivanov.start();
+        petrov.start();
+        Thread.sleep(2000);
+        isStoped = true;
+        Thread.sleep(1000);
+
+
+    }
+
+    public static class Stopwatch extends Thread {
+        private Runner owner;
+        private int stepNumber;
+
+        public Stopwatch(Runner runner) {
+            this.owner = runner;
+        }
+
+        public void run() {
+            try {
+                while (!isStoped) {
+                    doStep();
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void doStep() throws InterruptedException {
+            stepNumber++;
+            Thread.sleep(1000 / owner.speed);
+            System.out.println(owner.getName() + " делает шаг №" + stepNumber + "!");
+        }
+    }
+
+    public static class Runner {
+        Stopwatch stopwatch;
+        private String name;
+        private int speed;
+
+        public Runner(String name, int speed) {
+            this.name = name;
+            this.speed = speed;
+            this.stopwatch = new Stopwatch(this);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSpeed(){
+            return speed;
+        }
+
+        public void start() {
+            stopwatch.start();
+        }
+    }
+}
