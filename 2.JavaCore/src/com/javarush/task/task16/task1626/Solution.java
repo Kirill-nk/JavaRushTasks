291f5a0f8d51dc0d4d7e37f343512693b5d4a6ff
Kirill-nk <kirill9414@yandex.ru>
2024-06-09 10:34:40 +0300

    //1626. Создание по образцу.

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task16.task1626;
+
+/* 
+Создание по образцу
+*/
+
+public class Solution {
+    public static int number = 5;
+
+    public static void main(String[] args) {
+        new Thread(new CountdownRunnable(), "Уменьшаем").start();
+        new Thread(new CountUpRunnable(), "Увеличиваем").start();
+    }
+
+
+    public static class CountUpRunnable implements Runnable {
+        //Add your code here - добавь код тут
+        private int countIndexUp = 1;
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(500);
+                    System.out.println(toString());
+                    if (countIndexUp == number) return;
+                    countIndexUp++;
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
+    }
+
+
+    public static class CountdownRunnable implements Runnable {
+        private int countIndexDown = Solution.number;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexDown -= 1;
+                    if (countIndexDown == 0) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexDown;
+        }
+    }
+}
+
+//1626. Создание по образцу.
+//Вы решили задачу лучше, чем 3% учеников.
+//Вам удалось ее решить с 9 попытки.
+//Среднее количество попыток для этой задачи 3.37.
+//Всего эту задачу решили 33985 учеников.
