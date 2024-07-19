9d387f2d816c4dbe93bf7e0d4440c6a9116b298a
Kirill <kirill9414@yandex.ru>
2024-07-19 23:55:14 +0300

    1629. Только по-очереди!. Условие

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -11,20 +11,31 @@ import java.io.InputStreamReader;
 public class Solution {
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException {
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
 
-//        t1.printResult();
-//        t2.printResult();
+
+
+        t1.printResult();
+        t2.printResult();
     }
 
     //add your code here - добавьте код тут
     public static class Read3Strings extends Thread {
         @Override
         public void run() {
+        while (!isInterrupted())
+            try {
+                String a1 = reader.readLine();
+                String b1 = reader.readLine();
+                String c1 = reader.readLine();
+            } catch (IOException e){
+            }
+        }
+        public void printResult(){
             System.out.println();
         }
     }
