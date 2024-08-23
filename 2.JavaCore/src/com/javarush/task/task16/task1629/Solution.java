22ca7c09ce1698cf14b2bf96ed709d9deb2ee5a6
Kirill <kirill9414@yandex.ru>
2024-08-23 13:15:13 +0300

    1623. Условие

--- 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -3,6 +3,10 @@ package com.javarush.task.task16.task1629;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 /* 
 Только по-очереди!
@@ -16,17 +20,40 @@ public class Solution {
         Read3Strings t2 = new Read3Strings();
 
         //add your code here - добавьте код тут
+        t1.start();
+        t1.join();
+
+        t2.start();
+        t2.join();
 
         t1.printResult();
         t2.printResult();
-        t2.printResult();
     }
 
-    //add your code here - добавьте код тут
     public static class Read3Strings extends Thread {
-        @Override
-        public void run() {
+        private StringBuilder builder = new StringBuilder();
+        private List<String> result = new ArrayList<>();
 
+        public void run() {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    String str = reader.readLine();
+                    builder.append(str).append(" ");
+                    result.add(str);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void printResult() {
+            System.out.println(builder.toString().trim());
+            //String collectedResult = String.join(" ", result);
+            //System.out.println(collectedResult);
         }
     }
 }
+
+//1629. Только по-очереди!
+//Вы решили задачу лучше, чем 33% учеников. Вам удалось ее решить с 1 попытки. Среднее количество попыток для этой задачи 1.79. Всего эту задачу решили 32930 учеников.
