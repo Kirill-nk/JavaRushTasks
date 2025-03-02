e61ae9cb64be776b05c31bbebe95dc2a1e65e4fb
Kirill <kirill9414@yandex.ru>
2025-03-02 09:51:38 +0300

    1805 Уникальные байты с решением

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task18.task1805;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+Сортировка байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+
+        File file = new File(fileName);
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            Set<Integer> uniqueBytes = new HashSet<>();
+            int currentByte;
+            while ((currentByte = fileInputStream.read()) != -1) {
+                uniqueBytes.add(currentByte);
+            }
+            List<Integer> sortedBytes = new ArrayList<>(uniqueBytes);
+            Collections.sort(sortedBytes);
+
+            for (int b: sortedBytes){
+                System.out.printf(b + " ");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
