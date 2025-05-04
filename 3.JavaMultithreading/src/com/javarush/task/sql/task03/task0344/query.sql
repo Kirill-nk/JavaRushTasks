2221eb3f863fd97e9cb77b6aabdcc3e5d3052664
Kirill-nk <kirill9414@yandex.ru>
2025-05-04 11:05:01 +0300

    Задачки на SQL, 0344

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+select f.title
+ from films f
+    left join (select id, last_name
+               from film_directors
+               where last_name = 'Spielberg') director
+        on f.director_id = director.id
