6bab725a6c1d6cd257cf6c41a3e2f54c890df957
Kirill-nk <kirill9414@yandex.ru>
2025-11-17 22:01:16 +0300

    Racer: 12.28. Вы решили задачу лучше, чем 46% учеников. Вам удалось ее решить с 1 попытки. Среднее количество попыток для этой задачи 1.92. Всего эту задачу решили 1870 учеников.

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -1,10 +1,17 @@
 package com.javarush.games.racer.road;
 
 import com.javarush.games.racer.RacerGame;
+import com.javarush.engine.cell.Game;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoadManager {
     public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
     public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
+    private static final int FIRST_LANE_POSITION = 16;
+    private static final int FOURTH_LANE_POSITION = 44;
+    private List<RoadObject> items = new ArrayList<>();
 
     private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
         if (type == RoadObjectType.THORN) {
@@ -13,4 +20,14 @@ public class RoadManager {
             return null;
         }
     }
+
+    private void addRoadObject(RoadObjectType type, Game game) {
+        int x = game.getRandomNumber(FIRST_LANE_POSITION, FOURTH_LANE_POSITION);
+        int y = -1 * RoadObject.getHeight(type);
+        RoadObject newObject = createRoadObject(type, x, y);
+        if (newObject != null) {
+            items.add(newObject);
+        }
+    }
+
 }
