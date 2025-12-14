f00f911fc8d8c21edcd17d4d3100593e74e6de6e
Kirill <kirill9414@yandex.ru>
2025-12-14 03:01:40 +0300

    RacerGame 22/28 Вы решили задачу лучше, чем 2% учеников. Вам удалось ее решить с 5 попытки. Среднее количество попыток для этой задачи 1.51. Всего эту задачу решили 1711 учеников.

---
+++ 8.Games/src/com/javarush/games/racer/FinishLine.java
@@ -0,0 +1,19 @@
+package com.javarush.games.racer;
+
+public class FinishLine extends GameObject {
+    private boolean isVisible = false;
+    private FinishLine finishLine;
+
+    public FinishLine() {
+        super(RacerGame.ROADSIDE_WIDTH, -1 * ShapeMatrix.FINISH_LINE.length, ShapeMatrix.FINISH_LINE);
+    }
+
+    public void show(){
+        isVisible = true;
+    }
+
+    public void move(int boost){
+        if (!isVisible){return;}
+        y += boost;  // ← теперь работает
+    }
+}
